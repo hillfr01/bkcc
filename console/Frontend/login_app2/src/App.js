36e8821572780b9e85ab1e0b8a7e7833a13c78be
hillfr01 <76491115+hillfr01@users.noreply.github.com>
2023-10-10 19:41:21 -0400

    got simplest prop working

--- console/Frontend/login_app2/src/App.js
+++ console/Frontend/login_app2/src/App.js
@@ -4,12 +4,19 @@ import LoginForm from './LoginForm';
 import LoginAttemptList from './LoginAttemptList';
 
 const App = () => {
-  const [loginAttempts, setLoginAttempts] = useState([]);
+    const [loginAttempts, setLoginAttempts] = useState(0);
+    //const [loginUsername, setLoginUsername] = useState("");
+    //const [loginPassword, setLoginPassword] = useState("");
+
+    function updateAttempts(login, password) {
+        setLoginAttempts(prevloginAttempts => prevloginAttempts + 1);
+        alert(loginAttempts);
+    }
 
   return (
     <div className="App">
-      <LoginForm onSubmit={({ login, password }) => console.log({ login, password })} />
-      <LoginAttemptList attempts={loginAttempts} />
+      <LoginForm onSubmit={({ login, password }) => updateAttempts(login, password)} />
+          <LoginAttemptList loginAttempts={loginAttempts} />
     </div>
   );
 };
--- console/Frontend/login_app2/src/LoginAttemptList.js
+++ console/Frontend/login_app2/src/LoginAttemptList.js
@@ -3,14 +3,19 @@ import "./LoginAttemptList.css";
 
 const LoginAttempt = (props) => <li {...props}>{props.children}</li>;
 
-const LoginAttemptList = (props) => (
-	<div className="Attempt-List-Main">
-	 	<p>Recent activity</p>
-	  	<input type="input" placeholder="Filter..." />
-		<ul className="Attempt-List">
-			<LoginAttempt>Username was {props.username} and password was {props.password}</LoginAttempt>
-		</ul>
-	</div>
-);
+const LoginAttemptList = (props) => {
+	const loginAttempts = props.loginAttempts;
+	alert(JSON.stringify(props));
+
+	return (
+		<div className="Attempt-List-Main">
+			<p>Recent activity</p>
+			<input type="input" placeholder="Filter..." />
+			<ul className="Attempt-List">
+				<LoginAttempt>Attempt no. {loginAttempts}.</LoginAttempt>
+			</ul>
+		</div>
+	);
+}
 
 export default LoginAttemptList;
